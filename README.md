98021729d58d39d0683fb373539e17a6cdd63f31
developer <gage.russell.atx@gmail.com>
2022-12-01 08:09:40 -0600

    typos in shell script for testing localstack

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Quick test to see if localstack supports directory pattern: filename.ext/filename.ext
 appears to work
 ## steps:
-1. make kind-cluster
-2. make set-context
-3. make lock-localstack
-4. make release-localstack
-5. kubectl port-forward <pod> 4566:4566
-6. awslocal s3api create-bucket --bucket my-bucket
-7. awslocal s3 cp foo.txt s3://my-bucket/test-dir/foo.txt/foo.txt
-8. awslocal s3 ls s3://my-bucket/test-dir/foo.txt
-9. make cleanup
+1. `make kind-cluster`
+2. `make set-context`
+3. `make lock-localstack`
+4. `make release-localstack`
+5. `kubectl port-forward <pod> 4566:4566`
+6. `cd ./aws-local-files`
+7. `sh create.sh`
+8. `make cleanup`
 
 ## notes:
 1. must have kind installed
--- aws-local-files/create.sh
+++ aws-local-files/create.sh
@@ -5,11 +5,17 @@
 # kubectl port-forward <pod> 4566:4566
 # cd aws-local-files
 
+# file/dir name duplication
 awslocal s3api create-bucket --bucket my-bucket;
 awslocal s3 cp ./foo.txt s3://my-bucket/foo.txt;
 awslocal s3 cp ./foo.txt s3://my-bucket/foo.txt/foo.txt;
 awslocal s3 ls s3://my-bucket --recursive;
 
+# object tagging
+awslocal s3api put-object-tagging --bucket my-bucket --key foo.txt --tagging "{\"TagSet\": [{\"Key\": \"department\", \"Value\": \"accounting\"}]}";
+awslocal s3api get-object-tagging --bucket my-bucket --key foo.txt;
+
+# triggered lambda
 awslocal lambda create-function --function-name my-lambda --handler python-lambda-zip.test_lambda.lambda_handler --runtime python3.9 --zip-file fileb://python-lambda-zip.zip --role arn:aws:iam::000000000000:role/lambda-role;
 awslocal s3api put-bucket-notification-configuration --bucket my-bucket --notification-configuration file://s3-notif-config.json;
 awslocal s3 cp ./foo.txt s3://my-bucket/test-triggers/foo.txt;
